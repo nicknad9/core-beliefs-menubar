617bf98e74916fa13065a198e17ef974b594a1e4
NICK NADEAU <j.nicholas.nadeau@outlook.com>
2026-04-18 18:14:03 +0800

    wire AppDelegate: llm resolution, Principles menu item, window opener

--- Sources/CorePrinciples/main.swift
+++ Sources/CorePrinciples/main.swift
@@ -1,31 +1,4 @@
 import AppKit
-import CorePrinciplesLib
-
-class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusItem: NSStatusItem!
-    private var dataService: DataService?
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        do {
-            let appDb = try AppDatabase()
-            dataService = DataService(dbQueue: appDb.dbQueue)
-        } catch {
-            let alert = NSAlert()
-            alert.messageText = "Database Error"
-            alert.informativeText = "Could not initialize database: \(error.localizedDescription)"
-            alert.runModal()
-            NSApplication.shared.terminate(nil)
-            return
-        }
-
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusItem.button?.title = "CP"
-
-        let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        statusItem.menu = menu
-    }
-}
 
 let app = NSApplication.shared
 app.setActivationPolicy(.accessory)
