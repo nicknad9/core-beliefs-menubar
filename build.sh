41ae87059f0a530bc023be98ecd6459baa388f77
NICK NADEAU <j.nicholas.nadeau@outlook.com>
2026-04-18 16:31:00 +0800

    add build pipeline: Info.plist + build.sh
    swiftc compilation, hdiutil DMG creation, bundle ID com.coreprinciples.app

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+
+APP_NAME="Core Principles"
+BUILD_DIR="build"
+APP_BUNDLE="$BUILD_DIR/$APP_NAME.app"
+
+rm -rf "$APP_BUNDLE"
+mkdir -p "$APP_BUNDLE/Contents/MacOS"
+
+swiftc -o "$APP_BUNDLE/Contents/MacOS/$APP_NAME" main.swift
+cp Info.plist "$APP_BUNDLE/Contents/"
+
+echo "Built $APP_BUNDLE"
+
+# Create DMG
+DMG_PATH="$BUILD_DIR/$APP_NAME.dmg"
+rm -f "$DMG_PATH"
+hdiutil create -volname "$APP_NAME" -srcfolder "$APP_BUNDLE" -ov -format UDZO "$DMG_PATH"
+echo "Created $DMG_PATH"
